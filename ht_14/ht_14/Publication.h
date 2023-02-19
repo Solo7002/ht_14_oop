4a50f70ea00d7aea75746e963bd38afa3dd8dc8f
Solod Ihor <solod7002@gmail.com>
2023-02-19 11:03:35 +0200

    Created "Publication.h"

---
+++ ht_14/ht_14/Publication.h
@@ -0,0 +1,28 @@
+#pragma once
+#include<iostream>
+#include<string>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+class Publication
+{
+	string name;
+	string author;
+	int year;
+public:
+	Publication();
+	Publication(string name, string autor, int year);
+
+	void setName(string name);
+	void setAuthor(string author);
+	void setYear(int year);
+
+	string getName()const;
+	string getAuthor()const;
+	int getYear()const;
+
+	void show()const;
+};
+
